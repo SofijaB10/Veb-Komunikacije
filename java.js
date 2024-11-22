8b0002205644cd0ff69b3271d4449c346887729e
SofijaB10 <153387648+SofijaB10@users.noreply.github.com>
2024-11-22 09:54:35 +0100

    Update java.js

--- java.js
+++ java.js
@@ -95,4 +95,9 @@ document.addEventListener('DOMContentLoaded', function () {
       console.log("Facebook Like/Share button clicked!");
     }
   });
+ window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-972K80YZXL');
   
